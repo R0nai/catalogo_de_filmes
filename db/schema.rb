f071c1b9862cdc534d9d645d76cd4e17a9bf5728
Ronai Melo <amzronai@gmail.com>
2024-04-04 14:35:45 -0300

    Add relation between Directors and Genres

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_165652) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_04_173229) do
   create_table "directors", force: :cascade do |t|
     t.string "name"
     t.string "nationality"
     t.date "dob"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "genre_id", null: false
+    t.index ["genre_id"], name: "index_directors_on_genre_id"
   end
 
   create_table "genres", force: :cascade do |t|
@@ -25,4 +27,5 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_165652) do
     t.datetime "updated_at", null: false
   end
 
+  add_foreign_key "directors", "genres"
 end
